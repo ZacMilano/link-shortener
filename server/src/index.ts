8e5751a50f0da3146c58a139c66dcc4f7812a105
zacmonroe <zmonroe0820@gmail.com>
2022-08-24 16:33:02 -0500

    implemented algorithm to get new short-URL path

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,20 +11,30 @@ const cache = createRedisClient({ url: "redis://localhost:6379" });
 
 app.use(morganLogger("combined"));
 
+function isValidShortPath(path: string) {
+  return !["health", "shorten"].includes(path);
+}
+
 export function healthHandler(req: Request, res: Response) {
   res.send({ status: "available" });
 }
 
 app.get("/health", healthHandler);
 
-app.post("/shorten/:longUrl", (req: Request, res: Response) => {
+app.post("/shorten/:longUrl", async (req: Request, res: Response) => {
   const longUrl = req.params.longUrl;
-  // const shortenedPath = crypto.randomBytes();
+  let bytesLength = 8;
+  let shortenedPath = crypto.randomBytes(bytesLength).toString("base64url");
+  let cacheCheck = await cache.get(shortenedPath);
+
+  while (cacheCheck !== null && isValidShortPath(shortenedPath)) {
+    shortenedPath = crypto.randomBytes(++bytesLength).toString("base64url");
+    cacheCheck = await cache.get(shortenedPath);
+  }
+
   // TODO
-  // 1. Generate N random bytes, into base 64 string, S
-  // 2. While S is already found in the cache:
-  //   1. Generate ++N random bytes, into base 64 string S
-  // 3. Return http://host:port/${S} as redirect URL
+  // 3. Put S --> L (long URL) in cache
+  // 4. Return http://host:port/${S} as redirect URL
 });
 
 export const server = app.listen(PORT, () => {
