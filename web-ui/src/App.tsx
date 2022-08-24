2a55cd64547dcb95f8a254dbbd89012ee9d50703
zacmonroe <zmonroe0820@gmail.com>
2022-08-24 12:37:01 -0500

    added some basic markup & css

--- web-ui/src/App.tsx
+++ web-ui/src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import "./App.scss";
 
 function App() {
-  return <p>hello there</p>;
+  return (
+    <>
+      <header className="header">
+        <h1 className="header__title">ZALS</h1>
+        <p className="header__subtitle">Zac's Amazing Link Shortener</p>
+      </header>
+
+      <main>
+        <section className="input-section">
+          <h2 className="instructions">
+            Simply submit your URL below. We'll take care of the rest.
+          </h2>
+        </section>
+
+        <section className="output-section">
+          <a href="#">Output</a>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default App;
