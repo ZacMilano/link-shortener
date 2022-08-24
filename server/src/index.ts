1e983bcb1dfad0d84915cfea516f9ac218afedd5
zacmonroe <zmonroe0820@gmail.com>
2022-08-24 16:54:45 -0500

    added bodyParser, sending short URL!

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 import morganLogger from "morgan";
 import crypto from "crypto";
 import { createClient as createRedisClient } from "redis";
@@ -10,6 +12,7 @@ const app = express();
 const cache = createRedisClient({ url: "redis://localhost:6379" });
 
 app.use(morganLogger("combined"));
+app.use(cors());
 
 function isValidShortPath(path: string) {
   return !["health", "shorten"].includes(path);
@@ -21,8 +24,7 @@ export function healthHandler(req: Request, res: Response) {
 
 app.get("/health", healthHandler);
 
-app.post("/shorten/:longUrl", async (req: Request, res: Response) => {
-  const longUrl = req.params.longUrl;
+app.post("/shorten", bodyParser.json(), async (req: Request, res: Response) => {
   let bytesLength = 8;
   let shortenedPath = crypto.randomBytes(bytesLength).toString("base64url");
   let cacheCheck = await cache.get(shortenedPath);
@@ -32,11 +34,24 @@ app.post("/shorten/:longUrl", async (req: Request, res: Response) => {
     cacheCheck = await cache.get(shortenedPath);
   }
 
-  // TODO
-  // 3. Put S --> L (long URL) in cache
-  // 4. Return http://host:port/${S} as redirect URL
+  console.log(req.body);
+  let longUrl = req.body.longUrl;
+  if (!longUrl.startsWith("http")) {
+    longUrl = `http://${longUrl}`;
+  }
+  await cache.set(shortenedPath, longUrl);
+
+  const shortenedUrl = `http://localhost/${shortenedPath}`;
+
+  res.status(201);
+  res.send({
+    shortenedUrl,
+    originalLength: longUrl.length,
+    shortenedLength: shortenedUrl.length,
+  });
 });
 
 export const server = app.listen(PORT, () => {
+  cache.connect().then(() => console.log("Connected to Redis cache."));
   console.log(`Link Shortening server listening at http://${HOST}:${PORT}`);
 });
