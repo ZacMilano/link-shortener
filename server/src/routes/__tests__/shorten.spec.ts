455807cbbee8c43ed37aeb412dabef2e0bc6c54a
zacmonroe <zmonroe0820@gmail.com>
2022-08-25 12:56:43 -0500

    completed shorten tests up to response section

--- server/src/routes/__tests__/shorten.spec.ts
+++ server/src/routes/__tests__/shorten.spec.ts
@@ -13,17 +13,8 @@ describe("/shorten route", () => {
   let cryptoSpy: jest.SpyInstance;
   let cacheGetSpy: jest.SpyInstance;
   let cacheSetSpy: jest.SpyInstance;
-
-  const mockReq = {
-    body: {
-      longUrl: "http://example.com",
-    },
-  } as any as Request;
-
-  const mockRes = {
-    status: jest.fn(),
-    send: jest.fn(),
-  } as any as Response;
+  let mockReq: Request;
+  let mockRes: Response;
 
   beforeEach(async () => {
     cache = {
@@ -38,35 +29,43 @@ describe("/shorten route", () => {
     cacheGetSpy = jest.spyOn(cache, "get");
     cacheSetSpy = jest.spyOn(cache, "set");
     cacheDb = {};
-    handler = shortenHandler(cache);
+
     cryptoSpy = jest.spyOn(Crypto, "randomBytes");
+
+    mockReq = {
+      body: {
+        longUrl: "http://example.com",
+      },
+    } as any as Request;
+    mockRes = {
+      status: jest.fn(),
+      send: jest.fn(),
+    } as any as Response;
+
+    handler = shortenHandler(cache);
   });
 
   describe("shortenHandler", () => {
     describe("cache behavior", () => {
       describe("the first-generated path has not been seen yet", () => {
-        it("only `get`s from the cache once", async () => {
+        const bytes = Buffer.from([0x12]);
+
+        beforeEach(async () => {
           // Arrange
-          const bytes = Buffer.from([0x12]);
           cryptoSpy.mockReturnValue(bytes);
 
           // Act
           await handler(mockReq, mockRes);
+        });
 
+        it("only `get`s from the cache once", () => {
           // Assert
           expect(cacheGetSpy).toHaveBeenCalledWith(bytes.toString("base64url"));
           expect(cacheGetSpy).toHaveBeenCalledTimes(1);
           expect(cacheGetSpy).toHaveReturnedWith(null);
         });
 
-        it("only `set`s in the cache once", async () => {
-          // Arrange
-          const bytes = Buffer.from([0x12]);
-          cryptoSpy.mockReturnValue(bytes);
-
-          // Act
-          await handler(mockReq, mockRes);
-
+        it("only `set`s in the cache once", () => {
           // Assert
           expect(cacheSetSpy).toHaveBeenCalledWith(
             bytes.toString("base64url"),
@@ -75,14 +74,79 @@ describe("/shorten route", () => {
           expect(cacheSetSpy).toHaveBeenCalledTimes(1);
         });
       });
-      describe("the first-generated path has been seen before", () => {
-        it("`get`s from the cache until a novel string is generated", () => {});
 
-        it("only `set`s in the cache once", () => {});
+      describe("the first-generated path has been seen before", () => {
+        beforeEach(async () => {
+          // Arrange
+          const iterations = [
+            [0x12],
+            [0x12, 0x34],
+            [0x12, 0x34, 0x56],
+            [0x12, 0x34, 0x56, 0x78],
+          ].map((bytes) => Buffer.from(bytes));
+
+          iterations.forEach((buffer) => {
+            cacheDb[buffer.toString("base64url")] = "seen before!";
+            cryptoSpy.mockReturnValueOnce(buffer);
+          });
+
+          cryptoSpy.mockReturnValueOnce(
+            Buffer.from([0x12, 0x34, 0x56, 0x78, 0x90])
+          );
+
+          // Act
+          await handler(mockReq, mockRes);
+        });
+
+        it("`get`s from the cache until a novel string is generated", () => {
+          // Assert
+          expect(cacheGetSpy).toHaveBeenCalledTimes(5);
+          expect(cacheGetSpy).toHaveBeenLastCalledWith(
+            Buffer.from([0x12, 0x34, 0x56, 0x78, 0x90]).toString("base64url")
+          );
+        });
+
+        it("only `set`s in the cache once", () => {
+          // Assert
+          expect(cacheSetSpy).toHaveBeenCalledTimes(1);
+        });
       });
     });
 
-    it("adds 'http://' to the longUrl if not already present", () => {});
+    it("does not add 'http://' to the longUrl if already present", async () => {
+      // Arrange
+      const bytes = Buffer.from([0x12]);
+      cryptoSpy.mockReturnValue(bytes);
+      const mockUrl =
+        "http://example.com/test/path?search=queryyyy&params=populateddddd";
+      mockReq.body.longUrl = mockUrl;
+
+      // Act
+      await handler(mockReq, mockRes);
+
+      // Assert
+      expect(cacheSetSpy).toHaveBeenCalledWith(
+        bytes.toString("base64url"),
+        mockUrl
+      );
+    });
+
+    it("adds 'http://' to the longUrl if not already present", async () => {
+      // Arrange
+      const bytes = Buffer.from([0x12]);
+      cryptoSpy.mockReturnValue(bytes);
+      const mockUrl = "example.com/test/path?search=query&params=populated";
+      mockReq.body.longUrl = mockUrl;
+
+      // Act
+      await handler(mockReq, mockRes);
+
+      // Assert
+      expect(cacheSetSpy).toHaveBeenCalledWith(
+        bytes.toString("base64url"),
+        `http://${mockUrl}`
+      );
+    });
 
     describe("response", () => {
       it("sends 500 if there's no cache connection", () => {});
